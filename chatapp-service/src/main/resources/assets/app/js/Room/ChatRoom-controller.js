b77f9b5badb854902cbe1e094ef01927c9221af1
mpielvitori <martinpielvitori@gmail.com>
2015-07-01 03:54:26 -0300

    un poco más

--- chatapp-service/src/main/resources/assets/app/js/Room/ChatRoom-controller.js
+++ chatapp-service/src/main/resources/assets/app/js/Room/ChatRoom-controller.js
@@ -1,14 +1,20 @@
 'use strict';
 
 angular.module('chatapp')
-  .controller('ChatRoomController', ['$scope', '$modal', 'Room', '$routeParams','Message', 'ChatRoom', 'TestEntity',
-    function ($scope, $modal, Room, $routeParams, Message, ChatRoom, TestEntity) {
+  .controller('ChatRoomController', ['$scope', '$modal', 'Room', '$routeParams','Message', 'ChatRoom', 'TestEntity', '$interval',
+    function ($scope, $modal, Room, $routeParams, Message, ChatRoom, TestEntity, $interval) {
 
         $scope.init = function() {
             $scope.room = Room.get({id: $routeParams.idRoom});
             $scope.messages = TestEntity.get({id: $routeParams.idRoom});
             $scope.nickname = $routeParams.nickname;
-        }
+            $scope.login = Date.now();
+        };
+
+
+            $interval(function () {
+                $scope.messages = TestEntity.get({id: $routeParams.idRoom});
+            }, 2000);
 
         $scope.sendMessage = function (message) {
             var newMessage = {
